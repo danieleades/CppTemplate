0534a52d229ffc28ca6261665cb0330a4ad1d6ef
danieleades <33452915+danieleades@users.noreply.github.com>
2018-05-23 22:06:11 +0100

    also install script to /usr/local/bin

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cp NewCppProject ~/.local/share/nautilus/scripts/
+cp NewCppProject /usr/local/bin/
 chmod +x ~/.local/share/nautilus/scripts/NewCppProject
+chmod +x /usr/local/bin/NewCppProject
 rm -r -f ../CppTemplate
